cb5a79f524a3859d152677d9e40eabe5bb39052f
Trygvi <trygvi.laksafoss@gmail.com>
2023-03-19 12:31:59 -0700

    fix: added lint and --fix

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -3,5 +3,5 @@ export const pages = [
   { index: 1, name: 'skills.md', route: '/skills' },
   { index: 2, name: 'experience.md', route: '/experience' },
   { index: 3, name: 'education.md', route: '/education' },
-  { index: 4, name: 'projects.md', route: '/projects' },
-];
+  { index: 4, name: 'projects.md', route: '/projects' }
+]
